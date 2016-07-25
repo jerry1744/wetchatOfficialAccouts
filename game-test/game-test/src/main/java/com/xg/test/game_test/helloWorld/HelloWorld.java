853dfd47d46dd26c4610f0f01beb5f5fb3bee179
jerry <jerry1744@qq.com>
2016-07-25 17:14:10 +0800

    creat first Class

---
+++ game-test/game-test/src/main/java/com/xg/test/game_test/helloWorld/HelloWorld.java
@@ -0,0 +1,23 @@
+package com.xg.test.game_test.helloWorld;
+
+/**
+ *
+ * @auther qikai
+ * @date 2016年7月25日
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		String self_introduction_1 = "first please allow me introduce my self";
+		String self_introduction_2 = "I am a project for test but not only a test";
+		String self_introduction_3 = "you can code everything , and please write a detailed annocation";
+		String self_introduction_4 = "that's all , thanks";
+		
+		System.out.println(self_introduction_1);
+		System.out.println(self_introduction_2);
+		System.out.println(self_introduction_3);
+		System.out.println(self_introduction_4);
+	}
+}
